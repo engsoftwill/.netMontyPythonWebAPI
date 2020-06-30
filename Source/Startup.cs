3a5783b776af5d3a75d7411eeb91350771ada0a4
engsoftwill <engeletwill@gmail.com>
2020-06-30 12:56:00 -0300

    service automapper

--- Source/Startup.cs
+++ Source/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Codenation.Challenge.Models;
 using Codenation.Challenge.Services;
 using Microsoft.AspNetCore.Builder;
@@ -26,6 +27,7 @@ namespace Codenation.Challenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddDbContext<ScriptsContext>();
             services.AddScoped<IQuoteService, QuoteService>();
             services.AddScoped<IRandomService, RandomService>();
